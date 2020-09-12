4bbadb4bf6dc333bac290ab4517c2b7411796e0f
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-09-12 11:17:44 +0600

    checout page ok

--- src/components/checkout/Cart.js
+++ src/components/checkout/Cart.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 export const Cart = ({ cart, index, removeCart }) => {
-  const { name, image } = cart;
+  const { name, image, price } = cart;
   return (
     <div className="cart">
-      <p>{name}</p>
-      <img src={image} alt={name} />
-      <button onClick={() => removeCart(index)}>Remove Cart</button>
+      <div className="image-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="text-container">
+        <p>
+          <b>{name}</b>
+        </p>
+        <p className="price">
+          <b>${price}</b>
+        </p>
+        <button onClick={() => removeCart(index)}>Remove Cart</button>
+      </div>
     </div>
   );
 };
--- src/components/checkout/Checkouts.js
+++ src/components/checkout/Checkouts.js
@@ -2,6 +2,8 @@ import React, { useContext, Fragment } from "react";
 import Context from "../../context/Context";
 import { Cart } from "./Cart";
 
+import "./checkout.scss";
+
 const Checkouts = () => {
   const { carts, removeCart } = useContext(Context);
   let price = carts.reduce((result, prd) => result + prd.price, 0);
@@ -9,13 +11,17 @@ const Checkouts = () => {
     <div className="checkouts">
       <div className="header">
         <div className="left">
+          <h1>Banner</h1>
           <h4>Hello, Guest</h4>
           <h1>Your Shopping Basket</h1>
+          <hr />
         </div>
         <div className="right">
           <p>
             SubTotal ({carts.length}) : <b>${price}</b>
           </p>
+          <input type="checkbox" /> <p>This order contains a gift</p>
+          <button>Proceed to Checkout</button>
         </div>
       </div>
       <div className="cart-items">
