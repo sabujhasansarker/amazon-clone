42252b5ec82f1a498b66e8a18e0975bf6ae1af87
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-09-14 08:54:02 +0600

    #1

--- src/components/checkout/Checkouts.js
+++ src/components/checkout/Checkouts.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, Fragment } from "react";
 import { Link } from "react-router-dom";
 
 import Context from "../../context/Context";
@@ -11,28 +11,39 @@ const Checkouts = () => {
   let price = carts.reduce((result, prd) => result + prd.price, 0);
   return (
     <div className="checkouts container">
-      <div className="header">
-        <div className="left">
-          <h1>Banner</h1>
-          <h4>Hello, Guest</h4>
-          <h1>Your Shopping Basket</h1>
-          <hr />
-        </div>
-        <div className="right">
-          <p>
-            SubTotal ({carts.length}) : <b>${price}</b>
-          </p>
-          <input type="checkbox" /> <p>This order contains a gift</p>
-          <Link to="/payment" className="button">
-            Proceed to Checkout
-          </Link>
-        </div>
-      </div>
-      <div className="cart-items">
-        {carts.map((cart, index) => (
-          <Cart key={index} cart={cart} index={index} removeCart={removeCart} />
-        ))}
-      </div>
+      {carts.length >= 1 ? (
+        <Fragment>
+          <div className="header">
+            <div className="left">
+              {/* <h1>Banner</h1> */}
+              <h4>Hello, Guest</h4>
+              <h1>Your Shopping Basket</h1>
+              <hr />
+            </div>
+            <div className="right">
+              <p>
+                SubTotal ({carts.length}) : <b>${price}</b>
+              </p>
+              <input type="checkbox" /> <p>This order contains a gift</p>
+              <Link to="/payment" className="button">
+                Proceed to Checkout
+              </Link>
+            </div>
+          </div>
+          <div className="cart-items">
+            {carts.map((cart, index) => (
+              <Cart
+                key={index}
+                cart={cart}
+                index={index}
+                removeCart={removeCart}
+              />
+            ))}
+          </div>
+        </Fragment>
+      ) : (
+        <h1 className="text-center">No product in cart</h1>
+      )}
     </div>
   );
 };
